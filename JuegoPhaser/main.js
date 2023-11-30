b85438f6ec7e9e5fb9ed1f75567436a60d2bd76c
LonchaDeXope <142729532+LonchaDeXope@users.noreply.github.com>
2023-11-30 19:45:40 +0100

    Trampas, Pinchos, Organizacion de Codigo y Arreglos
    He implementado las trampas y los pinchos, además de organizar el código en funciones.

--- JuegoPhaser/main.js
+++ JuegoPhaser/main.js
@@ -1,6 +1,6 @@
 var config = {
     type: Phaser.AUTO,
-    width: 9000,
+    width: 10000,
     height: 600,
     physics: {
         default: 'arcade',
@@ -34,35 +34,42 @@ var updatedScore;
 var player1MidAir = false;
 var player2MidAir = false;
 
+var player;
+var player2;
+var coins;
+
 
 
 function preload() {
 
     this.load.image("pozo", "assets/pozo.png");
-    //this.load.image("amongus","assets/spritesheetPH.png")
     this.load.image("azulPlat", "assets/Blue.png")
     this.load.image("coin", "assets/coin.png")
     this.load.image("cameraTracker", "assets/EmptyPNG.png")
+    this.load.image("trampas", "assets/manoint.png")
+    this.load.image("pincho", "assets/hpIcon.png")
 }
 
 function create() {
 
     //Player1
-    player = this.physics.add.sprite(0, 0, "pozo");
+    player = this.physics.add.sprite(0, 0, "pozo").refreshBody();
     player.setScale(scaleX, scaleY)
     player.setBounce(0.2)
 
     player.setCollideWorldBounds(true);
 
     //Player2
-    player2 = this.physics.add.sprite(0, 400, "pozo");
+    player2 = this.physics.add.sprite(0, 400, "pozo").refreshBody();
     player2.setScale(scaleX, scaleY)
     player2.setBounce(0.2)
 
     player2.setCollideWorldBounds(true);
 
 
-    //Atributos de los personajes, y variables auxiliares
+    //Atributos de los personajes, y variables auxiliaresç
+    gameVelocity = 500;
+
     score1 = 0;
     score2 = 0;
 
@@ -81,6 +88,10 @@ function create() {
     vidasText1 = this.add.text(vidasXCoord, textos1YCoord, "Vidas 3", { fontSize: "32px", fill: '#000' })
     vidasText2 = this.add.text(vidasXCoord, textos2YCoord, "Vidas 3", { fontSize: "32px", fill: '#000' })
 
+    //DEBUG
+    textoDebug = this.add.text(800, 10, "Playerx: 0", { fontSize: "32px", fill: '#000' })
+    textoDebug.scrollFactorX = 0;
+
     //Mantener los textos en las esquinas
     scoreText1.scrollFactorX = 0
     scoreText1.scrollFactorY = 0
@@ -94,25 +105,14 @@ function create() {
     vidasText2.scrollFactorX = 0
     vidasText2.scrollFactorY = 0
 
-    //CREAR MONEDAS (PrimeraX, espacio entre ellas, numero que se genera)
-    //Coins
-    coins = this.physics.add.staticGroup()
-
-    GenerateCoins(coins, 800, 300, 20);
-
-    //Colisiones
-    this.physics.add.collider(player, coins, function (player, coin) {
-        coin.destroy();
-        addCoin1();
-    })
-
-    this.physics.add.collider(player2, coins, function (player2, coin) {
-        coin.destroy();
-        addCoin2();
-    })
+    //CREACION DEL MUNDO
+    //Creo los grupos
+    CrearGrupos(this);
+    //Creo los objetos
+    InicializarMundo(this)
 
     //CameraObject
-    camara = this.physics.add.sprite(config.width/2.05, 300, "cameraTracker");
+    camara = this.physics.add.sprite(config.width / 2.05, 300, "cameraTracker");
 
     camara.body.setAllowGravity(false)
     camara.setCollideWorldBounds(true);
@@ -181,10 +181,23 @@ function update() {
     }
 
 
-    player.setVelocityX(250)
-    player2.setVelocityX(250)
-    camara.setVelocityX(250)
+    player.setVelocityX(gameVelocity)
+    player2.setVelocityX(gameVelocity)
+    camara.setVelocityX(gameVelocity)
 
+    //Debug
+    textoDebug.text = "PlayerX = " + player.x;
+
+    //LOOP
+    if (player.x > 4500) {
+
+        player.x = 0;
+        player2.x = 0;
+        camara.x = config.width / 2.05;
+
+        InicializarMundo(this);
+
+    }
 
 }
 
@@ -204,16 +217,116 @@ function addCoin2() {
 
 }
 
-function GenerateCoins(coins, startingX, separation, number) {
+function player1GetDMG() {
+
+    vidas1--;
+    vidasText1.text = 'Vidas: ' + vidas1;
+
+}
+
+function player2GetDMG() {
+
+    vidas2--;
+    vidasText2.text = 'Vidas: ' + vidas2;
+}
+
+function GenerateCoins(contexto, startingX, separation, number) {
 
     for (let index = 0; index < number; index++) {
 
-        coins.create(startingX, 250, "coin").setScale(0.1).refreshBody()
-        coins.create(startingX, 550, "coin").setScale(0.1).refreshBody()
+        coins.create(startingX, 220, "coin").setScale(0.12).refreshBody()
+        coins.create(startingX, 520, "coin").setScale(0.12).refreshBody()
+
 
 
         startingX += separation;
     }
 
+    
+    //Colisiones
+    contexto.physics.add.collider(player, coins, function (player, coin) {
+        coin.destroy();
+        addCoin1();
+    })
+
+    contexto.physics.add.collider(player2, coins, function (player2, coin) {
+        coin.destroy();
+        addCoin2();
+    })
+
 }
 
+function GenerateTraps(contexto, startingX, separation, number) {
+
+    for (let index = 0; index < number; index++) {
+
+        traps.create(startingX, 220, "trampas").setScale(0.3).refreshBody()
+        traps.create(startingX, 520, "trampas").setScale(0.3).refreshBody()
+
+
+        startingX += separation;
+    }
+
+        //Colisiones con trampas
+        contexto.physics.add.collider(player, traps, function (player, trap) {
+
+            trap.destroy();
+            CreateSpikeTrapP1(contexto);
+    
+        })
+    
+        contexto.physics.add.collider(player2, traps, function (player2, trap) {
+    
+            trap.destroy();
+            CreateSpikeTrapP2(contexto);
+    
+        })
+
+}
+
+function CreateSpikeTrapP1(contexto) {
+
+    pinchos.create(player.x + screen.width + 50, 280, "pincho").setScale(1.7).refreshBody()
+
+    
+    //Colisiones con trampas
+    contexto.physics.add.collider(player, pinchos, function (player, pincho) {
+
+        pincho.destroy();
+        player1GetDMG();
+
+    })
+
+
+
+}
+
+function CreateSpikeTrapP2(contexto) {
+
+    pinchos.create(player2.x + screen.width + 50, 580, "pincho").setScale(1.7).refreshBody()
+
+    contexto.physics.add.collider(player2, pinchos, function (player2, pincho) {
+
+        pincho.destroy();
+        player2GetDMG();
+
+    })
+
+}
+
+function CrearGrupos(contexto){
+
+    //Creo los grupos fisicos
+    coins = contexto.physics.add.staticGroup()
+    traps = contexto.physics.add.staticGroup()
+    pinchos = contexto.physics.add.staticGroup();
+
+}
+
+function InicializarMundo(contexto){
+
+    GenerateCoins(contexto, 800, 1300, 5);
+    GenerateTraps(contexto, 1000, 2500, 3)
+
+
+}
